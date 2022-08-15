1fdd3da54400ced5894ce3dad6562d2f6fb56dd5
Marina <marina2013@tut.by>
2022-08-15 21:07:35 +0300

    feat: final decision

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
     constructor() {}
 
+    // this method accepts min and max value of range of number to guess
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
+    // this method returns solution candidate (you make an assumption based on range and previous assumptions)
     guess() {
-
+        return Math.round(((this.max - this.min) / 2) + this.min);
     }
 
+    // this method is called if prev call of `guess()` returned number which is greater than answer
     lower() {
-
+        return this.max = Math.round(((this.max - this.min) / 2) + this.min);
     }
 
+    // this method is called if prev call of `guess()` returned number which is lower than answer
     greater() {
-
+        return this.min = Math.round(((this.max - this.min) / 2) + this.min);
     }
 }
 
